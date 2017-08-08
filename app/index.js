cbdbd071ad330ffb89c531cb1d792cc33898ee63
kpicaza <kpicaza.sound@gmail.com>
2017-08-08 20:29:49 +0200

    add express pipeline, and minimal index.html

---
+++ app/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var timeService = require('./time/service');
+
+var app = express();
+
+process.env.PORT = 4200;
+
+app.get("/", function (request, response) {
+    response.sendFile(path.resolve('views/index.html'));
+});
+
+app.get("/:time", function (request, response) {
+    response.send(JSON.stringify(
+      timeService(request.params.time)
+    ));
+});
+
+
+var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
